c7ccc516737c32182b9edaf13ad886130df03da4
CristianMB <cristianjmb10@gmail.com>
2020-05-20 15:25:31 -0500

    print text depending on user selection

--- Practica_builder/src/practica_builder/Practica_builder.java
+++ Practica_builder/src/practica_builder/Practica_builder.java
@@ -5,24 +5,63 @@ package practica_builder;
 import Abstraccion.*;//del paquete abstraccion traemos todo
 import Productos .*;//del paquete productos traemos todo
 import Concrete_builder.*;//del paquete concrete_builder obtenemos todo
+import java.util.Scanner;
 public class Practica_builder {//creamos una clase llamada practica builder que sera el main
 
-    
+        private static Scanner reader = new Scanner(System.in);//nos ayudara a recibir todos los datos
+        
     public static void main(String[] args) {//realizamos el main
         // TODO code application logic here
+        
+        int eleccion;
+        
         construir_camion obj= new construir_camion();//instanciamos al director
         CamionBuilder obj1= new Camion_uno();//instanciamos la clase concreta y su referencia es la clase abstracta "CamionBuilder"
         CamionBuilder obj2=new Camion_dos();//instanciamos la clase concreta y su referencia es la clase abstracta "CamionBuilder"
+   
         
-        obj.setcamionBuilder(obj1);//llamamos al set de tipo camionbuilder y le damos el valor de obj1 o camion uno
-        obj.fabrica_camion();//llamamos el metodo fabrica_camion de la clase construir camion que nos ayudara a establecer los valores
+        do{
+           eleccion = menu();
+           switch(eleccion){
+               case 1:
+                   obj.setcamionBuilder(obj1);
+                   obj.fabrica_camion();
+                   imprimir(obj.getcamion());
+                   break;
+               case 2:
+                   obj.setcamionBuilder(obj2);
+                   obj.fabrica_camion();
+                   imprimir(obj.getcamion());
+                   break;
+               case 3:
+                   System.out.println("Saliendo del programa");
+                   break;
+               default:
+                   System.out.println("Has escogido un valor no valido");
+                   break;
+                   
+           }
+        }while(eleccion != 3);      
         
-        camion nueva=obj.getcamion();//creamos una variable de tipo "camion" que reciba al valor del getcamion()
-        nueva.getCarroceria();
-        nueva.getMotor();
-        nueva.getRuedas();
-    
     }
     
+    
+    public static void imprimir(camion nueva){//creamos una variable de tipo "camion" que reciba al valor del getcamion() 
+        System.out.println("Se ha elegido el camion de:\n"
+                + "Carroceria: "+nueva.getCarroceria()
+                + "\nMotor     : "+nueva.getMotor()
+                + "\nRuedas    : "+nueva.getRuedas());
+        
+    }
+    
+    
+    public static int menu(){
+        System.out.println("Que Camion deseas ver?: \n"
+                + "1. Camion uno\n"
+                + "2. Camion dos\n"
+                + "3. Salir \n"
+                + "Por favor selecciona uno: ");
+       return reader.nextInt();
+    }
     
 }
