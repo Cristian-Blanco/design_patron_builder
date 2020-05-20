5ce305463e491b647a748ff2f3532050dfb08910
CristianMB <cristianjmb10@gmail.com>
2020-05-20 15:42:26 -0500

    code commented

--- Practica_builder/src/practica_builder/Practica_builder.java
+++ Practica_builder/src/practica_builder/Practica_builder.java
@@ -13,55 +13,55 @@ public class Practica_builder {//creamos una clase llamada practica builder que 
     public static void main(String[] args) {//realizamos el main
         // TODO code application logic here
         
-        int eleccion;
+        int eleccion;//creamos una variable de modo entero
         
         construir_camion obj= new construir_camion();//instanciamos al director
         CamionBuilder obj1= new Camion_uno();//instanciamos la clase concreta y su referencia es la clase abstracta "CamionBuilder"
         CamionBuilder obj2=new Camion_dos();//instanciamos la clase concreta y su referencia es la clase abstracta "CamionBuilder"
    
         
-        do{
-           eleccion = menu();
-           switch(eleccion){
-               case 1:
-                   obj.setcamionBuilder(obj1);
-                   obj.fabrica_camion();
-                   imprimir(obj.getcamion());
-                   break;
-               case 2:
-                   obj.setcamionBuilder(obj2);
-                   obj.fabrica_camion();
-                   imprimir(obj.getcamion());
-                   break;
-               case 3:
-                   System.out.println("Saliendo del programa");
-                   break;
+        do{//realizamos un do while
+           eleccion = menu();//el entero eleccion sera igual a la funcion menu() que tambien es de tipo entero
+           switch(eleccion){//Realizamos un switch case para la condicion
+               case 1://si es el caso 1 entonces
+                   obj.setcamionBuilder(obj1);//llamamos el metodo setcamionBuilder y le enviamos el objeto camion uno
+                   obj.fabrica_camion();//llamamos el metodo que hara todo el cambio de informacion en camion
+                   imprimir(obj.getcamion());//llamamos la funcion imprimir y le enviamos como parametro el objeto camion
+                   break;//rompemos el switch
+               case 2://si es el caso 2 entonces
+                   obj.setcamionBuilder(obj2);//llamamos el metodo setcamionBuilder y le enviamos el objeto camion dos
+                   obj.fabrica_camion();//llamamos el metodo que hara todo el cambio de informacion en camion
+                   imprimir(obj.getcamion());//llamamos la funcion imprimir y le enviamos como parametro el objeto camion
+                   break;//rompemos el switch
+               case 3://si es el caso 3 entonces
+                   System.out.println("Saliendo del programa");//imprimimos texto
+                   break;//rompemos el switch
                default:
-                   System.out.println("Has escogido un valor no valido");
-                   break;
+                   System.out.println("Has escogido un valor no valido");//imprimimos texto
+                   break;//rompemos el switch
                    
            }
-        }while(eleccion != 3);      
+        }while(eleccion != 3);//mientras la "eleccion" es distinta a tres entonces...
         
     }
     
     
     public static void imprimir(camion nueva){//creamos una variable de tipo "camion" que reciba al valor del getcamion() 
-        System.out.println("Se ha elegido el camion de:\n"
-                + "Carroceria: "+nueva.getCarroceria()
-                + "\nMotor     : "+nueva.getMotor()
-                + "\nRuedas    : "+nueva.getRuedas());
+        System.out.println("Se ha elegido el camion de:\n"//imprimimos texto
+                + "Carroceria: "+nueva.getCarroceria()//imprimimos la carroceria a traves del get
+                + "\nMotor     : "+nueva.getMotor()//imprimimos la motor a traves del get
+                + "\nRuedas    : "+nueva.getRuedas());//imprimimos la ruedas a traves del get
         
     }
     
     
-    public static int menu(){
-        System.out.println("Que Camion deseas ver?: \n"
+    public static int menu(){//realizamos una funcion llamada menu que sera de tipo entero
+        System.out.println("Que Camion deseas ver?: \n"//imprimimos texto
                 + "1. Camion uno\n"
                 + "2. Camion dos\n"
                 + "3. Salir \n"
                 + "Por favor selecciona uno: ");
-       return reader.nextInt();
+       return reader.nextInt();//retornamos un numero seleccionado por el usuario
     }
     
 }
